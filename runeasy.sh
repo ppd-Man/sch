b7b347ef63bda4a170d83c49de3dc32cd0aeb84b
ppd-Man <it.test@wiidreamthinker.com>
2023-01-30 14:28:29 +0800

    Create runeasy.sh

---
+++ runeasy.sh
@@ -0,0 +1,5 @@
+# nohup /home/pi/runflask.sh &>/home/pi/sch/myflasky.log &
+# nohup /home/pi/runsendrpc.sh &>/home/pi/sch/mysendrpc.log &
+nohup /home/pi/runsendrpc.sh &>/home/pi/sch/mysendrpc.log &
+/home/pi/runflask.sh 
+
